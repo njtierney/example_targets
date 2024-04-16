9f0f3861e55525842755cbc419f4af1bb480c9ab
njtierney <nicholas.tierney@gmail.com>
2024-04-16 11:46:00 +1000

    using tar_parquet

--- _targets.R
+++ _targets.R
@@ -25,7 +25,19 @@ tar_plan(
       gap_data,
       "data/gap_data.parquet"
     )
-  )
+  ),
+  
+  sclfun <- function(red, path) {
+    tbl <- tibble::tibble(red = red)
+    write_parquet(tbl, path)
+    tbl
+  },
+    tar_target(srcs, data.frame(red = 1:10)),
+    tar_target(red, srcs$red),
+    tar_target(i, seq_len(nrow(srcs))),
+    ## I end up with three copies of the file vector, but only 'parquet' is monitored for file changes afaik
+    tar_target(path, sprintf("data/%03i.parquet", i)),
+    tar_parquet(files, sclfun(red, path), pattern = map(red, path))
 # target = function_to_make(arg), ## drake style
 
 # tar_target(target2, function_to_make2(arg)) ## targets style
